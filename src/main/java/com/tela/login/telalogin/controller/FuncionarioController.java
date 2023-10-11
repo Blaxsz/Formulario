4a4f76d8f38219e587603281e2a5b87cd007b273
Joao <joaovictor.gizidoro@pm.me>
2023-10-11 20:30:59 -0300

    something

--- src/main/java/com/tela/login/telalogin/controller/FuncionarioController.java
+++ src/main/java/com/tela/login/telalogin/controller/FuncionarioController.java
@@ -1,13 +1,27 @@
 package com.tela.login.telalogin.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.tela.login.telalogin.model.Funcionario;
+import com.tela.login.telalogin.repository.FuncionarioRepository;
 
 @Controller
 public class FuncionarioController {
 
+    @Autowired
+    FuncionarioRepository repository;
+
     @GetMapping("/aluno/cadastro")
     public String cadastroAluno() {
         return "cadastro";
+    }
+
+    @PostMapping("/cadastro")
+    public String cadastroFuncionario(Funcionario funcionario) {
+        repository.save(funcionario);
+        return "";
     }
 }
