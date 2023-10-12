c699437c87fec7b472f41fae5d9286d1b1def5c0
Joao <joaovictor.gizidoro@pm.me>
2023-10-11 21:17:40 -0300

    Lista de funcionarios criada

--- src/main/java/com/tela/login/telalogin/controller/FuncionarioController.java
+++ src/main/java/com/tela/login/telalogin/controller/FuncionarioController.java
@@ -1,9 +1,13 @@
 package com.tela.login.telalogin.controller;
 
+import java.util.ArrayList;
+
+import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.tela.login.telalogin.model.Funcionario;
 import com.tela.login.telalogin.repository.FuncionarioRepository;
@@ -22,6 +26,15 @@ public class FuncionarioController {
     @PostMapping("/cadastro")
     public String cadastroFuncionario(Funcionario funcionario) {
         repository.save(funcionario);
-        return "";
+        return "redirect:/funcionario/lista";
+    }
+
+    @GetMapping("/funcionario/lista")
+    public ModelAndView lista() {
+        ModelAndView mv = new ModelAndView("lista");
+        java.util.List<Funcionario> funcionarios = new ArrayList<>();
+        funcionarios = repository.findAll();
+        mv.addObject("funcionarios", funcionarios);
+        return mv;
     }
 }
