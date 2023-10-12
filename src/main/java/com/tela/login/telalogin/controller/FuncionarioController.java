8a57e845ab43e987b899aa0331796fba32a5a4bd
Joao <joaovictor.gizidoro@pm.me>
2023-10-11 22:06:44 -0300

    alguma coisa

--- src/main/java/com/tela/login/telalogin/controller/FuncionarioController.java
+++ src/main/java/com/tela/login/telalogin/controller/FuncionarioController.java
@@ -1,11 +1,12 @@
 package com.tela.login.telalogin.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
-import org.hibernate.mapping.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,7 +19,7 @@ public class FuncionarioController {
     @Autowired
     FuncionarioRepository repository;
 
-    @GetMapping("/aluno/cadastro")
+    @GetMapping("/funcionario/cadastro")
     public String cadastroAluno() {
         return "cadastro";
     }
@@ -36,5 +37,29 @@ public class FuncionarioController {
         funcionarios = repository.findAll();
         mv.addObject("funcionarios", funcionarios);
         return mv;
+    }
+
+    @GetMapping("/funcionario/excluir/{id}")
+    public String excluir(@PathVariable("id") int id) {
+        repository.deleteById(id);
+        return "redirect:/funcionario/lista";
+    }
+
+    @GetMapping("/funcionario/login")
+    public String login() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginFuncionario(Funcionario funcionario) {
+        List<Funcionario> funcionarios = new ArrayList<>();
+        funcionarios = repository.findAll();
+
+        for (Funcionario f : funcionarios) {
+            if (f.getLogin().equals(funcionario.getLogin()) && f.getSenha().equals(funcionario.getSenha())) {
+                return "redirect:/funcionario/lista";
+            }
+        }
+        return "redirect:/funcionario/login";
     }
 }
